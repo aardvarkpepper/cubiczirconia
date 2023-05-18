1168680b2b51ad80a6fd84736e8639003d266a6c
aardvarkpepper <aardvarkpepper@gmail.com>
2023-05-18 02:58:54 -0400

    Revert some data types, change some data sizes.

--- backend/db/schema.sql
+++ backend/db/schema.sql
@@ -13,16 +13,16 @@ CREATE TABLE users (
     user_id SERIAL PRIMARY KEY,
     user_login_name VARCHAR(40) NOT NULL,
     user_login_password VARCHAR(40) NOT NULL,
-    user_failed_logins TEXT NOT NULL,
-    user_last_login TEXT NOT NULL,
-    user_date_of_birth TEXT NOT NULL,
-    user_account_create_date TEXT NOT NULL,
+    user_failed_logins SMALLINT NOT NULL CHECK (user_failed_logins <= 0),
+    user_last_login DATE NOT NULL,
+    user_date_of_birth DATE NOT NULL,
+    user_account_create_date DATE NOT NULL,
     user_username VARCHAR(40) NOT NULL,
-    user_image_type VARCHAR(10) NOT NULL,
+    user_image_type VARCHAR(20) NOT NULL,
     user_image_local VARCHAR(80) NOT NULL,
     user_image_url VARCHAR(200) NOT NULL,
-    user_subscription_type VARCHAR(40) NOT NULL,
-    user_access_level TEXT NOT NULL,
+    user_subscription_type VARCHAR(20) NOT NULL,
+    user_access_level SMALLINT NOT NULL,
     user_email VARCHAR(60) NOT NULL,
     user_quote VARCHAR (200),
     user_notepad TEXT
@@ -30,12 +30,12 @@ CREATE TABLE users (
 
 CREATE TABLE themes (
     theme_id SERIAL PRIMARY KEY,
-    theme_name VARCHAR(20) NOT NULL,
-    theme_display_size VARCHAR(20) NOT NULL,
+    theme_name VARCHAR(40) NOT NULL,
+    theme_display_size VARCHAR(40) NOT NULL,
     theme_show_badges BOOLEAN NOT NULL,
     theme_palette VARCHAR(20) NOT NULL,
-    theme_font VARCHAR(20) NOT NULL,
-    theme_text_color VARCHAR(20) NOT NULL,
+    theme_font VARCHAR(40) NOT NULL,
+    theme_text_color VARCHAR(40) NOT NULL,
     theme_text_size VARCHAR(20) NOT NULL,
     theme_background VARCHAR(20) NOT NULL,
     user_id INT NOT NULL,
@@ -45,7 +45,7 @@ CREATE TABLE themes (
 CREATE TABLE badges (
     badge_id SERIAL PRIMARY KEY,
     badge_name VARCHAR(40) NOT NULL,
-    badge_description VARCHAR(80) NOT NULL,
+    badge_description VARCHAR(120) NOT NULL,
     badge_image_local VARCHAR(80) NOT NULL
 );
 
@@ -54,9 +54,9 @@ CREATE TABLE jas_users_badges (
     user_id INT NOT NULL,
     badge_id INT NOT NULL,
     jas_user_badge_display BOOLEAN NOT NULL,
-    jas_user_badge_image_type VARCHAR(10) NOT NULL,
+    jas_user_badge_image_type VARCHAR(20) NOT NULL,
     jas_user_badge_image_url VARCHAR(80) NOT NULL,
-    jas_user_badge_for VARCHAR(120) NOT NULL,
+    jas_user_badge_for VARCHAR(160) NOT NULL,
     jas_user_badge_date DATE NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users(user_id) ON UPDATE CASCADE ON DELETE CASCADE,
     FOREIGN KEY (badge_id) REFERENCES badges(badge_id) ON UPDATE CASCADE ON DELETE CASCADE
