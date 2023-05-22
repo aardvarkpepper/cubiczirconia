f9b4b290d89a266aa523e5159ef6472393254881
aardvarkpepper <aardvarkpepper@gmail.com>
2023-05-22 06:30:38 -0400

    Add user_access_level conditionally formatted text to Home.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,18 @@
+import { useContext } from 'react';
+import { UserContext } from '../contexts/UserContext';
 import Div from "../styledComponents/Div.jsx";
 
 const Home = () => {
+
+    const { user } = useContext(UserContext);
+
     return (
         <div>
             <div>Front End Home</div>
             <Div>
                 Text styled with component (change styling in settings, under user themes).
             </Div>
+            {(user.user_access_level > 2) ? "Conditionally rendered text for access levels > 2" : "Conditionally rendered text for access levels not > 2"}
         </div>
 
 
